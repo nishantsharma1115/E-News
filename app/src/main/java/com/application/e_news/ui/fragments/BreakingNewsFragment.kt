4a4685c242990ae4a89667c2c0fe7c2e9825c9bd
Nishant Sharma <heroesblood02@gmail.com>
2020-06-17 14:58:47 +0530

    Navigation Added

---
+++ app/src/main/java/com/application/e_news/ui/fragments/BreakingNewsFragment.kt
@@ -0,0 +1,7 @@
+package com.application.e_news.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.application.e_news.R
+
+class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {
+}
---
+++ app/src/main/java/com/application/e_news/ui/fragments/SearchNewsFragment.kt
@@ -0,0 +1,7 @@
+package com.application.e_news.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.application.e_news.R
+
+class SearchNewsFragment : Fragment(R.layout.fragment_search_news) {
+}
